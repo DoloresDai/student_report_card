96a59b5fb82af8b0918a3156e06675d0edbcbd1d
DoloresDai <841856753@qq.com>
2019-03-15 15:24:49 +0800

    Complete all functions.

--- src/main/java/report_card/Main.java
+++ src/main/java/report_card/Main.java
@@ -1,4 +1,206 @@
 package main.java.report_card;
 
+import main.java.report_card.model.Student;
+
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        mainMenu();
+    }
+
+    public static void mainMenu() {
+        menu();
+        select(new Scanner(System.in).next());
+    }
+
+    public static void menu() {
+        System.out.println("1. 添加学生\n" +
+                "2. 生成成绩单\n" +
+                "3. 退出\n" +
+                "请输入你的选择（1～3）：");
+    }
+
+    public static void select(String in) {
+        switch (in) {
+            case "1":
+                System.out.println("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：");
+                Scanner input = new Scanner(System.in);
+                String content = input.nextLine();
+                if (isCorrectInput(content)) {
+                    saveStudent(newStudent(getInput(content)));
+                    mainMenu();
+                } else {
+                    System.out.println("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：");
+                    select("1");
+                }
+                break;
+            case "2":
+                System.out.println("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
+                Scanner output = new Scanner(System.in);
+                String out = output.nextLine();
+                if (isCorrectOutput(out)) {
+                    System.out.println(getList(getSelected(out)));
+                    saveList(getSelected(out));
+                } else {
+                    System.out.println("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
+                    select("2");
+                }
+                break;
+            case "3":
+                System.out.println("已退出");
+                break;
+        }
+    }
+
+    public static String[] getInput(String in) {
+        return in.split("，");
+    }
+
+    public static void saveStudent(Student student) {
+        ObjectOutputStream o = null;
+        try {
+            o = new ObjectOutputStream((new FileOutputStream("student.txt",true)));
+            o.writeObject(student);
+            o.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("学生" + student.getName() + "的成绩被添加");
+            if (o != null) {
+                try {
+                    o.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void saveList(List<Student> list) {
+        BufferedWriter o = null;
+        try {
+            o = new BufferedWriter(new FileWriter("list.txt"));
+
+            o.write(getList(list));
+            o.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (o != null) {
+                try {
+                    o.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static double getTotalScore(List<Student> list) {
+        return list.stream().map(x -> x.getTotal_score()).reduce(0.00, (x, y) -> x + y);
+    }
+
+    public static double getAverage(List<Student> list) {
+        return list.stream().map(x -> x.getAverage()).reduce(0.00, (x, y) -> x + y);
+    }
+
+    public static String getList(List<Student> list) {
+        return "成绩单\r\n" +
+                "姓名|数学|语文|英语|编程|平均分|总分 \r\n" +
+                "========================\r\n" +
+                Arrays.toString(getAllStudent(list))
+                + "\r\n========================\r\n" +
+                "全班总分平均数：" + getTotalScore(list)
+                + "\r\n全班总分中位数：" + getAverage(list);
+    }
+
+    public static String[] getAllStudent(List<Student> list) {
+        String[] detail = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            Student student = list.get(i);
+            detail[i]=student.getName() + "|" + student.getSubject().get("数学") +
+                    "|" + student.getSubject().get("语文") +
+                    "|" + student.getSubject().get("英语") +
+                    "|" + student.getSubject().get("编程") +
+                    "|" + student.getAverage() +
+                    "|" + student.getTotal_score();
+        }
+        return detail;
+    }
+
+    public static List<Student> getSelected(String in) {
+        String[] ids = getInput(in);
+        List<Student> list = new ArrayList<>();
+        for (String s : ids) {
+            list.add(ifStudent(s));
+        }
+        return list;
+    }
+
+    public static Student ifStudent(String id) {
+        List<Student> students = getStudent();
+        for (Student s : students) {
+            if (s.getId().equals(id)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public static List<Student> getStudent() {
+        ObjectInputStream i = null;
+        List<Student> students = new ArrayList<>();
+        try {
+            i = new ObjectInputStream(new FileInputStream("student.txt"));
+            while (i.available()>0) {
+                Student student = (Student) i.readObject();
+                students.add(student);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (i != null) {
+                try {
+                    i.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return students;
+    }
+
+    public static boolean isCorrectInput(String in) {
+        String regex = "[\\u4E00-\\u9FA5]{1,4}\\uff0c(0|[1-9][0-9]?|100)\\uff0c[\\u4E00-\\u9FA5]{2}\\uff1a(0|[1-9][0-9]?|100)\\uff0c[\\u4E00-\\u9FA5]{2}\\uff1a(0|[1-9][0-9]?|100)\\uff0c[\\u4E00-\\u9FA5]{2}\\uff1a(0|[1-9][0-9]?|100)\\uff0c[\\u4E00-\\u9FA5]{2}\\uff1a(0|[1-9][0-9]?|100)";
+        return in.matches(regex);
+    }
+
+    public static boolean isCorrectOutput(String out) {
+        String regex = "^(0|[1-9][0-9]?)(，\\d+)*$";
+        return out.matches(regex);
+    }
+
+    public static double getSubject(String[] student, String subject) {
+        for (String s : student) {
+            if (s.startsWith(subject)) {
+                return Double.parseDouble(s.split("：")[1]);
+            }
+        }
+        return 0;
+    }
+
+    public static Student newStudent(String[] strings) {
+        TreeMap<String, Double> sss = new TreeMap<>();
+        sss.put("数学", getSubject(strings, "数学"));
+        sss.put("语文", getSubject(strings, "语文"));
+        sss.put("英语", getSubject(strings, "英语"));
+        sss.put("编程", getSubject(strings, "编程"));
+        return new Student(strings[0], strings[1], sss);
+    }
+
 }
+
--- src/main/java/report_card/model/Student.java
+++ src/main/java/report_card/model/Student.java
@@ -1,16 +1,18 @@
 package main.java.report_card.model;
 
+import java.io.Serializable;
+import java.util.Iterator;
 import java.util.TreeMap;
-import java.util.stream.Stream;
 
-public class Student {
+public class Student implements Serializable, Comparable {
+    private static final long serializableUID = 15645445551L;
     private String name;
-    private long id;
-    private TreeMap<String, Long> subject;
-    private long total_score;
-    private long average;
+    private String id;
+    private TreeMap<String, Double> subject;
+    private double total_score;
+    private double average;
 
-    public Student(String name, long id, TreeMap<String, Long> subject) {
+    public Student(String name, String id, TreeMap<String, Double> subject) {
         this.name = name;
         this.id = id;
         this.subject = subject;
@@ -24,44 +26,83 @@ public class Student {
         this.name = name;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public TreeMap<String, Long> getSubject() {
+    public TreeMap<String, Double> getSubject() {
         return subject;
     }
 
-    public void setTotal_score(long total_score) {
-        this.total_score = total_score;
+    public double countTotalScore() {
+
+        Iterator<Double> i = getSubject().values().iterator();
+        double sum = 0;
+        while (i.hasNext()) {
+            sum += i.next();
+        }
+        return sum;
+//         Stream.of(getSubject().value s().iterator()).map(Iterator::next).reduce( 0,(x, y) -> x + y);
     }
 
-    public void countTotalScore() {
-        long sum = Stream.of(getSubject()).map(x ->
-                x.get(x.entrySet().iterator().next())).reduce((x, y) -> x + y).get();
-        setTotal_score(sum);
+    public void setTotal_score() {
+        this.total_score = countTotalScore();
     }
 
-    public long getTotal_score() {
+    public double countAverage() {
+        return average = getTotal_score() / getSubject().size();
+    }
+
+    public void setAverage() {
+        this.average = countAverage();
+    }
+
+    public double getTotal_score() {
+        setTotal_score();
         return total_score;
     }
 
-    public void setAverage(long average) {
-        this.average = average;
-    }
-
-    public void countAverage() {
-        long average;
-        average = getTotal_score() / getSubject().size();
-        setAverage(average);
-    }
-
-    public long getAverage() {
+    public double getAverage() {
+        setAverage();
         return average;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Student s = (Student) obj;
+        return getId() == s.getId();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Student) {
+            Student s = (Student) o;
+            return this.name.compareTo(s.name);
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return name + "|" + subject.get("数学") +
+                "|" + subject.get("语文") +
+                "|" + subject.get("英语") +
+                "|" + subject.get("编程") +
+                "|" + getAverage() +
+                "|" + getTotal_score();
+
+    }
 }
